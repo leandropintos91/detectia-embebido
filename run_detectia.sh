305b09bb22828ae442cc532d2be96d5049621e23
Leandro Pintos <leopintos11@gmail.com>
2023-10-02 21:28:58 -0300

    #171 - Pasar captura de GPS a hilo

--- run_detectia.sh
+++ run_detectia.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source tflite2-env/bin/activate
 sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
-python ./script_deteccion/TFLite_detection_webcam.py --bypass_speed
+python3 ./script_deteccion/TFLite_detection_webcam.py --verbose
 
 
