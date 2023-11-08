3eac4cdabd6773566b35dc4741a29a811c1701e5
Leandro Pintos <leopintos11@gmail.com>
2023-11-08 12:39:02 -0300

    #000 quit sudo start up of gps

--- run_detectia.sh
+++ run_detectia.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 source /home/$USER/detectia-embebido/detectia-env/bin/activate
-sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
+#sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
 python3 /home/$USER/detectia-embebido/script_deteccion/TFLite_menu_detection_webcam.py --verbose
